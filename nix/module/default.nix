d8ecfd418fe3fc08befd94fd3fae8377cd429242
Ivan Balashov <ivanbalashov239@gmail.com>
2025-10-15 14:08:33 +0800

    fix nix

--- nix/module/default.nix
+++ nix/module/default.nix
@@ -13,14 +13,13 @@ in
 
     configs = mkOption {
       type = types.listOf (types.submodule {
-        options = rec {
+        options = {
           path = mkOption {
             type = types.path;
             description = "Directory to monitor for sync conflict files";
           };
           name = mkOption {
             type = types.str;
-            default = replaceStrings [ "/" ] [ "_" ] (toString path);
             description = "Name for the systemd service, defaults to path with slashes replaced by underscores";
           };
           passwordFile = mkOption {
